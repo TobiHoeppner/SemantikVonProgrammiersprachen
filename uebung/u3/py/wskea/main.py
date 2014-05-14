4dcc4a3d7755ac7a4d3e935f1a7f7107578eb2b9
Tobias Höppner <thoeppner@thoeppner.de>
2014-05-14 21:53:18 +0200

    [mod] u3 changes 8)

--- uebung/u3/py/wskea/main.py
+++ uebung/u3/py/wskea/main.py
@@ -1,43 +1,49 @@
 __author__ = 'TH'
 
-w = []
+# anfang
+w = ['x']
 s = []
-k = ['add', 'read', 'read']
+k = ['output', '=', 1, 1, 'not', True, 'add', 'read', 'read', 'skip', 'assign', 42]
 e = [1, 1]
 a = []
 test = [w, s, k, e, a]
 
 
-def run(machine):
-    # check if input is useable
-    if len(machine) > 5:
-        print('Wrong machine!')
-        return 0
+def run(machine):   # delta
     w = machine[0]  # process stack
     s = machine[1]  # variable store
     k = machine[2]  # cmd stack
     e = machine[3]  # input (file)
     a = machine[4]  # output (file)
-    # if len(e) == 0:
-    #     print('empty input!')
-    #     return 0
     i = 0
     while len(k) > 0:
         cmd = k.pop()
         if type(cmd) == int:
             w.append(cmd)
+        elif type(cmd) == bool:
+            w.append(cmd)
+        elif cmd == 'not':
+            b = not w.pop()
+            w.append(b)
+        elif cmd == '=':
+            w.append(w.pop() == w.pop())
         elif cmd == 'read':
-            # from first position on input to first position on process stack
             w.append(e.pop())
-        elif cmd == 'add' or '+':
+        elif cmd == 'add' or cmd == '+':
             w.append(w.pop() + w.pop())
-        elif cmd == 'sub' or '-':
+        elif cmd == 'sub' or cmd == '-':
             w.append(w.pop() - w.pop())
-        elif cmd == 'mul' or '*':
+        elif cmd == 'mul' or cmd == '*':
             w.append(w.pop() * w.pop())
-        elif cmd == 'div' or '/':
+        elif cmd == 'div'or cmd == '/':
             w.append(w.pop() / w.pop())
-        print('run '+str(i)+': '+str(machine))
+        elif cmd == 'skip':
+            pass
+        elif cmd == 'assign':
+            s.append((w.pop(), w.pop()))
+        elif cmd == 'output':
+            a.append(w.pop())
+        print('run ' + str(i) + ': ' + str(machine))
         i += 1
     print('success after '+str(i)+' iterations !')
     return 1
